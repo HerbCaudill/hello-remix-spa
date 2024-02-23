2638a65d445b6eed880103fa8f58dfb2938f9d10
Herb Caudill <herb@devresults.com>
2024-02-23 14:22:49 +0100

    working locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-  plugins: [remix({ ssr: false }), tsconfigPaths()],
-});
+  plugins: [remix({ ssr: false }), tsconfigPaths(), wasm(), topLevelAwait()],
+})
